36163d763546c10dec306e425369c599218a4d84
luucanclini <luucanclini@gmail.com>
2025-11-28 13:21:28 -0300

    implementacion de auth.middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,37 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
 import { AuthService } from '../models/AuthService.Class';
 
-// Extendemos la interfaz de Express para incluir al usuario en la Request
-// Esto evita el uso de @ts-ignore en los controladores
 declare global {
     namespace Express {
         interface Request {
             user?: {
                 id: string;
                 role: string;
-            };
+            } | null;
         }
     }
 }
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-    // Clean Code: Extraer el header de forma segura
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // "Bearer TOKEN"
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
-        return res.status(401).json({ message: 'Acceso denegado: Falta el token' });
+        return res.status(401).json({ error: 'Unauthorized',message: 'Acceso denegado: Falta el token' });
     }
 
-    const decodedUser = AuthService.verifyToken(token);
-
-    if (!decodedUser) {
-        return res.status(403).json({ message: 'Token inválido o expirado' });
+    try {
+        const userData = AuthService.verifyToken(token);
+        if (!userData) {
+            return res.status(403).json({ error: 'Forbidden', message: 'Token inválido o expirado' });
+        }
+        req.user = userData;
+        next();
+        
+    } catch (error) {
+        return res.status(403).json({ error: 'Forbidden', message: 'Token inválido o expirado' });
     }
-
-    // ¡ACÁ ESTÁ LA MAGIA! ✨
-    // Guardamos el usuario decodificado en la request
-    req.user = decodedUser;
-
-    next();
 };
